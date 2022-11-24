525556bcc3034145b9a08313bf5b9ebce991b52d
Fostrankill <romain.pourchaux@gmail.com>
2022-11-24 20:30:31 +0100

    Add DataCorner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,9 @@ import MainHall from "@pages/MainHall/MainHall";
 import DoorKey from "@pages/DoorKey/DoorKey";
 import Projector from "@pages/Projector/Projector";
 import Office from "@pages/Office/Office";
+import DataCorner from "pages/DataCorner/DataCorner";
+import FridgeOpen from "./pages/FridgeOpen/FridgeOpen";
+import FridgeClose from "./pages/FridgeClose/FridgeClose";
 
 function App() {
   return (
@@ -23,19 +26,19 @@ function App() {
           <Route path="/main-hall" element={<MainHall />} />
           <Route path="/wild-door" element={<WildDoor />} />
           <Route path="/wc" element={<WC />} />
-          {/* <Route path="/backyard" element={<Backyard />} /> */}
+          <Route path="/backyard" element={<Backyard />} />
           <Route path="/door-key" element={<DoorKey />} />
           <Route path="/first-floor" element={<FirstFloor />} />
           <Route path="/projector" element={<Projector />} />
           <Route path="/office" element={<Office />} />
-          {/* <Route path="/wild-hall" element={<WildHall />} /> */}
-          {/* <Route path="/loic" element={<Loic />} />
+          <Route path="/wild-hall" element={<WildHall />} />
+          <Route path="/loic" element={<Loic />} />
           <Route path="/table-js" element={<TableJS />} />
           <Route path="/data-corner" element={<DataCorner />} />
           <Route path="/fridge-open" element={<FridgeOpen />} />
           <Route path="/fridge-close" element={<FridgeClose />} />
           <Route path="/cafe-cabinet" element={<CafeCabinet />} />
-          <Route path="/wc-mirror" element={<WcMirror />} /> */}
+          <Route path="/wc-mirror" element={<WcMirror />} />
         </Routes>
       </div>
     </Router>
