784b4afb463ce45379629e43d995d12c2c09ec7e
guenk <gueguenk@gmail.com>
2022-11-25 11:22:00 +0100

    adding backyard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,16 +7,13 @@ import TableJS from "@pages/TableJS/TableJS";
 import MainHall from "@pages/MainHall/MainHall";
 import WildDoor from "@pages/WildDoor/WildDoor";
 import WildHall from "@pages/WildHall/WildHall";
-import Homepage from "@pages/Home";
 import DataCorner from "@pages/DataCorner/DataCorner";
 import FridgeOpen from "@pages/FridgeOpen/FridgeOpen";
 import FridgeClose from "@pages/FridgeClose/FridgeClose";
-import LoicMonster from "@pages/LoicMonster/LoicMonster";
 import Backyard from "@pages/Backyard/Backyard";
 import Monster from "@pages/Backyard/monster/Monster";
 import Loose from "@pages/Loose/Loose";
 import EndPage from "@pages/EndPage/EndPage";
-import BuildingDoor from "@/pages/BuildingDoor/BuildingDoor";
 import DoorCode from "@pages/DoorCode/DoorCode";
 import WC from "@pages/WC/WC";
 import CafeCabinet from "@pages/CafeCabinet/CafeCabinet";
@@ -25,6 +22,7 @@ import DoorKey from "@pages/DoorKey/DoorKey";
 import Projector from "@pages/Projector/Projector";
 import Office from "@pages/Office/Office";
 import LoicMonster from "@pages/LoicMonster/LoicMonster";
+import BuildingDoor from "./pages/BuildingDoor/BuildingDoor";
 import Loic from "./pages/Loic/Loic";
 import Timer from "./components/Timer/Timer";
 import { QuestContextProvider } from "./contexts/QuestObject";
@@ -49,7 +47,7 @@ function App() {
             <Route path="/wild-door" element={<WildDoor gotkey={gotkey} />} />
             <Route path="/backyard" element={<Backyard />} />
             <Route path="/monster" element={<Monster />} />
-            {/* <Route path="/backyard" element={<Backyard />} /> */}
+
             <Route
               path="/door-key"
               element={<DoorKey gotkey={gotkey} setGotKey={setGotKey} />}
@@ -67,7 +65,6 @@ function App() {
             <Route path="/fridge-close" element={<FridgeClose />} />
             <Route path="/micro-close" element={<MicroClose />} />
             <Route path="/end-page" element={<EndPage />} />
-            {/* <Route path="/wc-mirror" element={<WcMirror />} /> */}
           </Routes>
         </div>
       </Router>
