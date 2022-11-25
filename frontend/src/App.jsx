e88c0ec9abd866c1d9ef73fc92b25b96c53c33a8
hswiergiel <h.swiergiel@hotmail.fr>
2022-11-25 11:58:50 +0100

    merging dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,12 @@ import TableJS from "@pages/TableJS/TableJS";
 import MainHall from "@pages/MainHall/MainHall";
 import WildDoor from "@pages/WildDoor/WildDoor";
 import WildHall from "@pages/WildHall/WildHall";
-import Homepage from "@pages/Home";
 import DataCorner from "@pages/DataCorner/DataCorner";
 import FridgeOpen from "@pages/FridgeOpen/FridgeOpen";
 import FridgeClose from "@pages/FridgeClose/FridgeClose";
 import LoicMonster from "@pages/LoicMonster/LoicMonster";
 import Loose from "@pages/Loose/Loose";
 import EndPage from "@pages/EndPage/EndPage";
-import BuildingDoor from "@/pages/BuildingDoor/BuildingDoor";
 import DoorCode from "@pages/DoorCode/DoorCode";
 import WC from "@pages/WC/WC";
 import CafeCabinet from "@pages/CafeCabinet/CafeCabinet";
@@ -22,8 +20,9 @@ import FirstFloor from "@pages/FirstFloor/FirstFloor";
 import DoorKey from "@pages/DoorKey/DoorKey";
 import Projector from "@pages/Projector/Projector";
 import Office from "@pages/Office/Office";
-import LoicMonster from "@pages/LoicMonster/LoicMonster";
-import Loic from "./pages/Loic/Loic";
+import Loic from "@pages/Loic/Loic";
+import BuildingDoor from "@pages/BuildingDoor/BuildingDoor";
+import Backyard from "@pages/Backyard/Backyard";
 import Timer from "./components/Timer/Timer";
 import { QuestContextProvider } from "./contexts/QuestObject";
 import MicroClose from "./pages/MicroClose/MicroClose";
@@ -45,7 +44,7 @@ function App() {
             <Route path="/table-js" element={<TableJS />} />
             <Route path="/main-hall" element={<MainHall />} />
             <Route path="/wild-door" element={<WildDoor gotkey={gotkey} />} />
-            {/* <Route path="/backyard" element={<Backyard />} /> */}
+            <Route path="/backyard" element={<Backyard />} />
             <Route
               path="/door-key"
               element={<DoorKey gotkey={gotkey} setGotKey={setGotKey} />}
@@ -63,7 +62,6 @@ function App() {
             <Route path="/fridge-close" element={<FridgeClose />} />
             <Route path="/micro-close" element={<MicroClose />} />
             <Route path="/end-page" element={<EndPage />} />
-            {/* <Route path="/wc-mirror" element={<WcMirror />} /> */}
           </Routes>
         </div>
       </Router>
